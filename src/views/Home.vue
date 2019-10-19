474f974d558af4c6fe242094b1ebe548a42bd750
ZCG-ZCG <545095602@qq.com>
2019-10-19 17:47:52 +0800

    init github

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <el-form :model="codeForm">
+      <el-form-item label="">
+        <el-input v-model="code" placeholder="请输入提取码"></el-input>
+      </el-form-item>
+      <el-form-item label="" style="margin-top:10px;">
+        <el-button type="primary">确定</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "home",
+  data() {
+    return {
+      codeForm: {
+        code: ''
+      }
+    }
+  },
+};
+</script>
+
+<style lang="stylu" scoped>
+  
+</style>
